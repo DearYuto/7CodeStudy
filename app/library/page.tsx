92c9b8be1bb38a5ceb5fb0802290dbc32d1f9573
DearYuto <reasons@kakao.com>
2024-04-21 00:02:42 +0900

    feat: library 페이지 로직 추가

--- app/library/page.tsx
+++ app/library/page.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
 
+import { Playlist } from '@/types';
+
+import PagePadding from '@/components/elements/PagePadding';
+import Category from './components/Category';
+import PlayListCard from '@/components/PlayListCard';
+
+import { getRandomElementFromArray } from '@/lib/utils';
+import { dummyPlaylistArray } from '@/lib/dummyData';
+
+
 const LibraryPage = () => {
-  return <div>LibraryPage</div>;
+  return (
+    <PagePadding>
+      <div className='mt-9'></div>
+      <Category />
+      <div className='mt-12'></div>
+      <section className='grid grdi-col gap-6 md:grid-cols-4 xl:grid-colos-3 2xl:grid-cols-6'>
+        <PlayListCard
+          playlist={getRandomElementFromArray(dummyPlaylistArray) as Playlist}
+        />
+      </section>
+    </PagePadding>
+  );
 };
 
 export default LibraryPage;
