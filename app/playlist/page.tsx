8fcc08ff9226927641c508fd049c690d1724e256
DearYuto <reasons@kakao.com>
2024-04-21 00:02:54 +0900

    feat: playlist 페이지 로직 추가

--- app/playlist/page.tsx
+++ app/playlist/page.tsx
@@ -1,7 +1,44 @@
+import PagePadding from '@/components/elements/PagePadding';
+import HeaderBgChanger from '@/components/HeaderBgChanger';
+import PlayListHead from '@/components/PlayListHead';
+import SongCardRowExpand from '@/components/SongCardRowExpand';
+import { getPlaylistById } from '@/lib/dummyData';
+import { getRandomElementFromArray } from '@/lib/utils';
+import { Playlist, Song } from '@/types';
+import { permanentRedirect } from 'next/navigation';
 import React from 'react';
 
-const PlaylistPage = () => {
-  return <div>PlaylistPage</div>;
+interface PlaylistPageProps {
+  searchParams: {
+    list: string;
+  };
+}
+
+const PlaylistPage = async (props: PlaylistPageProps) => {
+  const playlist = await getPlaylistById(Number(props.searchParams.list));
+
+  if (!playlist) {
+    permanentRedirect('/');
+  }
+
+  const imageSrc = (getRandomElementFromArray(playlist.songList) as Song)
+    ?.imageSrc;
+
+  return (
+    <PagePadding>
+      <HeaderBgChanger imageSrc={imageSrc} />
+      <div className='mt-12'></div>
+
+      <PlayListHead playlist={playlist} />
+      <div className='mt-12'></div>
+
+      <section className='flex flex-col gap-2'>
+        {playlist.songList.map((song, idx) => {
+          return <SongCardRowExpand song={song} key={idx} />;
+        })}
+      </section>
+    </PagePadding>
+  );
 };
 
 export default PlaylistPage;
